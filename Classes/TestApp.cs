f97150362169c3fd7e13524c2c2f011d18ab8253
Rahul Mer <info@bascrm.com>
2024-04-18 22:29:19 +0530

    Add project files.

---
+++ Classes/TestApp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TestSimultaneousTexts.Classes
+{
+    public class TestApp
+    {
+        public static int instance_counter = 0;
+        
+        public static async Task ReceiveInboundText()
+        {
+            // Write to a text file
+            string filePath = @"C:\Users\SLIM5\Desktop\ReceiveInboundTextLog.txt";
+            using (StreamWriter writer = File.AppendText(filePath))
+            {
+                writer.WriteLine($"{DateTime.Now} ReceiveInboundText triggered, instance_counter = {instance_counter}");
+                instance_counter++;
+                writer.WriteLine($"{DateTime.Now} Added 1 to instance_counter, it now equals {instance_counter}");
+            }
+
+            // Simulate delay
+            await Task.Delay(30000);
+        }
+    }
+}
---
+++ Controllers/TwilioController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using TestSimultaneousTexts.Classes;
+using Microsoft.Extensions.Logging;
+
+
+[Route("api/[controller]")]
+[ApiController]
+public class TwilioController : ControllerBase
+{
+    private readonly ILogger<TwilioController> _logger;
+
+    public TwilioController(ILogger<TwilioController> logger)
+    {
+        _logger = logger;
+    }
+    [HttpPost]
+    public async Task<IActionResult> ReceiveMessage()
+    {
+        // Log the incoming request
+        _logger.LogInformation("Received Twilio webhook request.");
+
+        // Log request content
+        using (StreamReader reader = new StreamReader(Request.Body))
+        {
+            string requestBody = await reader.ReadToEndAsync();
+            _logger.LogInformation($"Request body: {requestBody}");
+        }
+
+        // Assuming the Twilio webhook will call this endpoint
+        await Task.Run(() => TestApp.ReceiveInboundText());
+        return Ok();
+
+    }
+}
